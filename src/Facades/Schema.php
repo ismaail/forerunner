5061d1f3feb693c5ace819e3b931e716f8bc35cf
ismaail <contact@ismaail.com>
2026-03-21 17:29:10 +0000

    Pint fixes

--- src/Facades/Schema.php
+++ src/Facades/Schema.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Blaspsoft\Forerunner\Facades;
 
+use Blaspsoft\Forerunner\Schema\Struct;
 use Illuminate\Support\Facades\Facade;
 
 /**
  * @method static \Blaspsoft\Forerunner\Schema\Struct define(string $name, string $description, callable $callback)
  *
- * @see \Blaspsoft\Forerunner\Schema\Struct
+ * @see Struct
  */
 class Schema extends Facade
 {
